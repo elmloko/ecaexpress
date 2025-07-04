ec6642378042d8117ea7fd8ff5b8200201770eee
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2025-07-04 14:55:32 -0400

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-inline">
         {{ date('Y') }} Agencia Boliviana de Correos ©
-        <strong>GECA</strong> Todos los derechos reservados.
+        <strong>UNIENVIO</strong> Todos los derechos reservados.
     </div>
     Todos los derechos reservados.<strong>ecaExpress</strong>
     <a href="mailto:mespinozarojas46@gmail.com" class="opacity-75" title="Marco Antonio Espinoza Rojas">Copyright © MAER
